e8657ca93ec04ccecf7f011b8654813b58152fe6
liukun1016 <liukun1016@gmail.com>
2015-07-07 18:33:45 -0400

    Remove Main App

--- src/kunliu/MainApp.java
+++
@@ -1,9 +0,0 @@
-package kunliu;
-
-public class MainApp {
-
-	public static void main(String[] args) {
-		String input = "/Users/Kun/Git/IDE-CodeChallenge/test/0.txt";
-		Utility.parseWordCountInput(input);
-	}
-}
