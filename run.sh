447c6029918b20b7b30108e93318efb47f5d0a70
Kun <liukun1016@gmail.com>
2015-07-09 20:31:41 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 javac src/*java
 java -cp src/ TweetWordCount tweet_input/tweets.txt tweet_output/ft1.txt
 java -cp src/ RunningMedian tweet_input/tweets.txt tweet_output/ft2.txt
+
+#To try the "distributed" mode, adding a -d|D flag to the end:
+#java -cp src/TweetWordCount tweet_input/tweets tweet_output/ft1 -d
